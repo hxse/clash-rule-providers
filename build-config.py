2bfd50f9b2860e4a8960275e2045d4abbf474cdd
hxse <narutohy@163.com>
2023-02-22 00:44:23 +0800

    update yaml set_udp

--- build-config.py
+++ build-config.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 # coding: utf-8
-import yaml
 import requests
 from pathlib import Path
+import yaml
+from yaml import CSafeLoader as Loader, CSafeDumper as Dumper
 
-def set_udp(path='./profiles/proxies'):
-    for i in Path(path).glob('*.yaml'):
+
+def set_udp(path="./profiles/proxies"):
+    for i in Path(path).glob("*.yaml"):
         with open(i, "r", encoding="utf8") as f:
-            data = yaml.safe_load(f)
-            for item in data['proxies']:
-                item['udp']=True
+            data = yaml.load(f, Loader=Loader)
+            for item in data["proxies"]:
+                item["udp"] = True
         with open(i, "w", encoding="utf8") as f:
-            yaml.dump(data, f, allow_unicode=True)
+            yaml.dump(data, f, allow_unicode=True, Dumper=Dumper)
+
 
 set_udp()
 
 with open("config.custom.yaml", "r", encoding="utf8") as f:
-    custom_data = yaml.safe_load(f)
+    custom_data = yaml.load(f, Loader=Loader)
 
 with open("config.template.yaml", "r", encoding="utf8") as f:
-    template_data = yaml.safe_load(f)
+    template_data = yaml.load(f, Loader=Loader)
     template_providers = template_data["proxy-providers"]
     custom_providers = custom_data["proxy-providers"]
     for template_name in template_providers:
@@ -45,7 +48,7 @@ with open("config.template.yaml", "r", encoding="utf8") as f:
             template_data[i] = custom_data[i]
 
 with open("config.yaml", "w", encoding="utf8") as f:
-    yaml.dump(template_data, f, allow_unicode=True)
+    yaml.dump(template_data, f, allow_unicode=True, Dumper=Dumper)
 
 
 def reload_config(path, url="http://127.0.0.1:9090/configs"):
